63785ce10e02b297d3198f847cc09740f71d0015
le01s <nkbotcman@gmail.com>
2025-07-28 10:30:22 +0300

    new build

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 def home():
     return "Hello from CI/CD Pipeline!1"
 
+
+@app.route("/test")
+def new_build():
+    return "New build"
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
